453c6e0de405ba94975bbde92fc6bf6f9598cc15
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-02-16 17:48:17 -0500

    added some validation

--- client/src/routes/manage.js
+++ client/src/routes/manage.js
@@ -11,31 +11,51 @@ const loginStyle = {
 };
 
 export default function Manage() {
+    //set states
     const [name, setName] = useState("");
     const [quantity, setQuantity] = useState("");
     const [category, setCategory] = useState("");
+    const [validated, setValidated] = useState(false);
 
-    const onSubmit = async (e) => { 
-        e.preventDefault(); 
-        const product = { 
-            name, 
-            quantity, 
-            category, 
-        }; 
-        console.log(product);
-        alert('Product Created: ' + name);
-    
-        await axios.post("http://localhost:5000/api/addProduct", 
-            product)
-        .then((res) => console.log(res));
+    const clearState = () => {
+        setName("");
+        setQuantity("");
+        setCategory("");
+    };
 
+    //handle sumbit plus validation
+    const handleSubmit = async (event) => { 
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+            
+        setValidated(true);
+        if(form.checkValidity() === true){
+            event.preventDefault(); 
+            const product = { 
+                name, 
+                quantity, 
+                category, 
+            }; 
+            console.log(product);
+            alert('Product Created: ' + name);
+        
+            await axios.post("http://localhost:5000/api/addProduct", 
+                product)
+            .then((res) => console.log(res));
 
+            setValidated(false);
+            clearState();
+        };
     };
 
     return (
         <div>
             <h4 style={{textAlign: 'center', padding: '10px'}}>ADD:</h4>
             <div style={loginStyle}>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
                         <Form.Label>Product Name: </Form.Label>
                         <Form.Control 
@@ -44,7 +64,11 @@ export default function Manage() {
                             placeholder="Product" 
                             className="form-control"
                             onChange={(e) => setName(e.target.value)}
+                            required
                             />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a product name.
+                        </Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
@@ -55,23 +79,35 @@ export default function Manage() {
                             placeholder="quantity" 
                             className="form-control"
                             onChange={(e) => setQuantity(e.target.value)}
+                            required
                             />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a quantity.
+                        </Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Category:</Form.Label>
                         <Form.Control 
-                            type="text"
-                            name="category" 
-                            placeholder="category" 
+                            required
+                            as="select"
+                            type="select"
                             className="form-control"
                             onChange={(e) => setCategory(e.target.value)}
-                            />
+                        >
+                            <option value="">--</option>
+                            <option value="Bulk">Bulk</option>
+                            <option value="Canned">Canned</option>
+                            <option value="Produce">Produce</option>
+                            <option value="Misc">Misc</option>
+                        </Form.Control>
+                        <Form.Control.Feedback type="invalid">
+                            Please select a category.
+                        </Form.Control.Feedback>
                     </Form.Group>
 
-                    
-                    
-                    <Button className="form-control mb-3" type="submit" onClick={onSubmit}>ADD</Button>
+                    <Button className="form-control mb-3" type="submit">ADD</Button>
+                    </Form>
                 </div>
         </div>
     );
