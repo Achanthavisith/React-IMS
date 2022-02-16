bd0e44ef824ac8f8111a9951e63d3f246a08b9e0
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-02-16 14:59:30 -0500

    rename some directories
    renamed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
-const routerUrls = require('./routes/routes')
+const routerUrls = require('./controllers/routes')
 
 //config dotenv to access .env with DB address
 dotenv.config();
