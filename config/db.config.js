3b8ca388dadf72426804718bdd66d3cfe37482b0
MendokusaiCode <coussan52@gmail.com>
2022-02-23 18:12:58 -0500

    Revert "Merge branch 'Cal' into Andrew"
    This reverts commit 8a8afa3e1d1fd71ae3e7fe7ce893119d7133a1ec, reversing
    changes made to 0f25eaeb4a827949804a918c6857057daf41dacd.

--- config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: "admin",
-    PORT: "admin@cluster0.opzmr.mongodb.net",
-    DB: "react-ims?retryWrites=true&w=majority"
-  };
-
-  // have to figure out how to connect here
