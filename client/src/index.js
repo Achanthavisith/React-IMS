d29bf580d14b2308787d4094f859ad6ca480a078
MendokusaiCode <coussan52@gmail.com>
2022-04-23 16:47:04 -0400

    home page

--- client/src/index.js
+++ client/src/index.js
@@ -10,19 +10,36 @@ import {
 import Manage from './routes/manage';
 import Admin from './routes/admin';
 import Login from './routes/login';
+import Home from './routes/home';
 import reportWebVitals from './reportWebVitals';
+import EditableRow from './components/EditableRow';
+import ReadOnlyRow from './components/ReadOnlyRow';
 
 ReactDOM.render(
   <React.StrictMode>
+    
     <BrowserRouter>
+
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="manage" element={<Manage />}/>
         <Route path="admin" element={<Admin />} />
         <Route path="login" element={<Login />} />
+        <Route
+        index
+        element={ 
+          <Home style={{ padding: "1rem" }}>
+      
+      
+          </Home>
+        }
+      />
       </Route>
+      
     </Routes>
+    
   </BrowserRouter>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/routes/home.js
@@ -0,0 +1,79 @@
+import { Button, Form } from 'react-bootstrap';
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { UserContext } from '../context/context';
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+
+export default function Home() {
+
+    const {user} = useContext(UserContext);
+
+    const loginStyle = {
+        font: '15px arial sans',
+        margin: 'auto',
+        width: '60%',
+        padding: '5px',
+        marginTop: '10px',
+    }
+
+    return (
+        <div>
+            <Card className="text-center">
+  <Card.Header>Welcome to our Inventory Manager, </Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+  
+  <Carousel variant="dark"
+  border = 'black'
+style={loginStyle}
+ interval = {null}
+ class = "border border-primary"
+>
+  <Carousel.Item interval={null}>
+      
+    <img
+      className="d-block w-100 h-50"
+      src="https://www.simpleimageresizer.com/_uploads/photos/c2326972/264-2640106_inventory-management-system-logo-hd-png-download_1_500x250.png"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h5>First slide label</h5>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={null}>
+    <img
+      className="d-block w-100"
+      src="https://www.simpleimageresizer.com/_uploads/photos/c2326972/Warehouse_800x355.png"
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h5>Second slide label</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={null}>
+    <img
+      className="d-block w-100"
+      src="https://www.simpleimageresizer.com/_uploads/photos/c2326972/inventory_800x355.png"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h5>Third slide label</h5>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+  <Card.Footer className="text-muted">2 days ago</Card.Footer>
+</Card>
+
+
+      </div>
+    );
+}
